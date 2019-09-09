ce25b343a3446bdd6f2853accd35c67959630872
andri <andriwicaksonost@gmail.com>
2019-09-09 17:04:43 +0700

    Adding middleware, register middleware
    http://localhost:8008/admin/home?age=20

--- lumenapi/routes/web.php
+++ lumenapi/routes/web.php
@@ -20,44 +20,11 @@ $router->get('/key', function(){
 	return str_random(32);
 });
 
-
-// Basic Route
-$router->get('/get-method', function(){
-	return "This is GET Method";
-});
-
-$router->post('/post-method', function(){
-	return "This is POST Method";
-});
-
-
-// Basic Route Parameter
-$router->get('/user/{id}', function($id){
-	return "User ID : " . $id;
-});
-
-$router->get('/post/{postId}/comments/{commentId}', function ($postId, $commentId){
-	return 'Post ID : ' . $postId . '. Comment ID : ' . $commentId;
-});
-
-// Optional Route Parameter
-$router->get('/optional[/{param}]', function($param = null){
-	return $param;
-});
-
-$router->get('profile', function(){
-	return redirect()->route('route.profile');
-});
-
-$router->get('/profile/idstack', ['as' => 'route.profile', function(){
-	return route('route.profile');
+$router->get('/admin/home', ['middleware' => 'age', function(){
+	return 'Old Enough';
 }]);
 
-$router->group(['prefix' => 'admin'], function () use ($router){
-	$router->get('home', function(){
-		return 'Home Admin';
-	});
+$router->get('/fail', function(){
+	return 'Not yet mature';
 });
-
-
 
