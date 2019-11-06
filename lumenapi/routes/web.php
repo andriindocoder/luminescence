0fc1cbddaa2ca313b32ac72431521ef4724ad510
Andri Sedania <andri@sedaniainnovator.com>
2019-11-06 21:14:04 +0700

    Routes web for AuthorController

--- lumenapi/routes/web.php
+++ lumenapi/routes/web.php
@@ -15,6 +15,14 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/key', function(){
-	return str_random(32);
+$router->group(['prefix' => 'api'], function () use ($router) {
+  $router->get('authors',  ['uses' => 'AuthorController@showAllAuthors']);
+
+  $router->get('authors/{id}', ['uses' => 'AuthorController@showOneAuthor']);
+
+  $router->post('authors', ['uses' => 'AuthorController@create']);
+
+  $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
+
+  $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
 });
